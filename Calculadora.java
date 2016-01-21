c1050c04f76570ec060dc5119106fb9db1f92447
unknown <you@example.com>
2016-01-21 11:26:27 +0100

    Creado el metodo sumValues()

--- Calculadora.java
+++ Calculadora.java
@@ -7,18 +7,26 @@
  */
 public class Calculadora
 {
-   
+    //guarda el valor de los multiplos
     private int multiplos;
+    //guarda el numero de sumas realizadas;
+    private int suma;
+    //guarda el numero;
+    private int num;
+    //guarda el total de la suma
+    private int total;
     /**
      * Constructor for objects of class Calculadora
      */
     public Calculadora()
     {
         multiplos = 10;
+        suma = 0;
+        num = 0;
     }
 
     /**
-     * 
+     * Metodo que muestra por pantalla todos los multiplos de 5 del 10 al 95
      */
     public void multiplosOfFive()
     {
@@ -27,5 +35,18 @@ public class Calculadora
             multiplos = multiplos + 5;
             System.out.println(multiplos);
         }
+    }
+    /**
+     * Metodo que suma todos los valores del 0 al 10
+     */
+    public void sumValues()
+    {
+        while (suma < 10 )
+        {
+            num++;
+            suma++;
+            total = total + num;
+        }
+        System.out.println(total);
     }
 }
